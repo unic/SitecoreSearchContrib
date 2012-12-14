ebeb9b21892f6e79dfb0025d59898d94c1c179f1
Tobias Studer <studert@live.com>
2012-12-14 11:06:19 +0100

    Added additional FieldCrawlers

---
+++ scSearchContrib.Crawler/FieldCrawlers/CheckboxFieldCrawler.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Search.Crawlers.FieldCrawlers;
+
+namespace scSearchContrib.Crawler.FieldCrawlers
+{
+    /// <summary>
+    /// The checkbox field crawler.
+    /// </summary>
+    public class CheckboxFieldCrawler : FieldCrawlerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckboxFieldCrawler" /> class.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        public CheckboxFieldCrawler(Field field) : base(field) { }
+
+        /// <summary>
+        /// Returns true/false as textstring for checkbox field values
+        /// </summary>
+        /// <returns>string "true" or "false" (instead of "0" or "1"</returns>
+        public override string GetValue()
+        {
+            return this._field.Value == "1" ? "true" : "false";
+        }
+    }
+}
---
+++ scSearchContrib.Crawler/FieldCrawlers/PdfMediaFieldCrawler.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Search.Crawlers.FieldCrawlers;
+using scSearchContrib.Searcher.Utilities;
+
+namespace scSearchContrib.Crawler.FieldCrawlers
+{
+    /// <summary>
+    /// PdfMediaFieldCrawler extends FieldCrawlerBase from the Sitecore.Search 
+    /// API to provide text streams of a given PDF document.
+    /// </summary>
+    public class PdfMediaFieldCrawler : FieldCrawlerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PdfMediaFieldCrawler" /> class.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        public PdfMediaFieldCrawler(Field field) : base(field) { }
+
+        /// <summary>
+        /// Extracts text from pdf and returns that text
+        /// </summary>
+        /// <returns>Text of a pdf as string</returns>
+        public override string GetValue()
+        {
+            if (_field.Item.Paths.IsMediaItem)
+            {
+                MediaItem mediaItem = _field.Item;
+                if (mediaItem != null && mediaItem.MimeType.Equals(PdfUtil.PdfMimetype))
+                {
+                    return PdfUtil.ParsePdf(mediaItem);
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
